263600071c8c0238821b5646b59a7d7e43fabea0
Lalith <lalithpunepalli@gmail.com>
2020-03-21 19:19:27 +0530

    3 views ui completed

---
+++ Agora-iOS Swiftui/View/CreateElectionView3.swift
@@ -0,0 +1,21 @@
+//
+//  CreateElectionView3.swift
+//  Agora-iOS Swiftui
+//
+//  Created by Lalith on 20/03/20.
+//  Copyright © 2020 NANI. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateElectionView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateElectionView3_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateElectionView3()
+    }
+}
---
+++ Agora-iOS Swiftui/View/CreateElectionView4.swift
@@ -0,0 +1,21 @@
+//
+//  CreateElectionView4.swift
+//  Agora-iOS Swiftui
+//
+//  Created by Lalith on 21/03/20.
+//  Copyright © 2020 NANI. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateElectionView4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateElectionView4_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateElectionView4()
+    }
+}
